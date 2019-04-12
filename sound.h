16b2507acf69766c4810ce4a3747c90b4cf0ef1c
TakyMalika <e1801588@edu.vamk.fi>
2019-04-12 09:49:23 +0100

    Created testTone

--- sound.h
+++ sound.h
@@ -2,6 +2,8 @@
 //#define DEBUG
 #define RATE 16000		// samples per second
 #define CMD "arecord -r16000 -c1 -f S16_LE -d1 -q test.wav"
+#define PI 3.1415926
+
 // data structures
 struct WAVHDR{
 	char ChunkID[4];	// IT HAS TO BE "RIFF"
@@ -24,4 +26,4 @@ struct WAVHDR{
 // function declarations
 void displayWAVHDR(struct WAVHDR h);
 void displayWAVDATA(short []);
-// function definitions
+void testTone(int, int, float);
